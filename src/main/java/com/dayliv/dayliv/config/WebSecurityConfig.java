86808852db7816865e5c703d4dded5be67f025a8
yassir78 <yassir.acaf@gmail.com>
2021-03-16 20:13:22 +0100

    update proerties

--- src/main/java/com/dayliv/dayliv/config/WebSecurityConfig.java
+++ src/main/java/com/dayliv/dayliv/config/WebSecurityConfig.java
@@ -74,7 +74,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.authenticationEntryPoint(new RestAuthenticationEntryPoint())
 				.and()
 			.authorizeRequests()
-				.antMatchers("/", "/error", "/api/all", "/api/auth/**","/dayliv-api/auth/**", "/oauth2/**","/dayliv-api/product/**","/dayliv-api/image/**","/dayliv-api/partenaire/**").permitAll()
+				.antMatchers("/", "/error", "/api/all", "/api/auth/**","/dayliv-api/auth/**", "/oauth2/**","/dayliv-api/product/**","/dayliv-api/image/**","/dayliv-api/partenaire/**","/dayliv-api/commande/**").permitAll()
 			.anyRequest()
 				.authenticated()
 				.and()

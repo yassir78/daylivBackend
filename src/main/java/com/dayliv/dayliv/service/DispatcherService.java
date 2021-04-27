892f4fca0852a4fe42ec9982bb44717eb6fed81c
belkoweb <belkoweb9718@gmail.com>
2021-04-27 20:49:38 +0000

    dispatcher added

--- src/main/java/com/dayliv/dayliv/service/DispatcherService.java
+++ src/main/java/com/dayliv/dayliv/service/DispatcherService.java
@@ -3,10 +3,20 @@ package com.dayliv.dayliv.service;
 import java.util.List;
 
 import com.dayliv.dayliv.model.Dispatcher;
+import com.dayliv.dayliv.model.Partenaire;
 
 public interface DispatcherService {
 	public List<Dispatcher> findAll();
-
 	public Dispatcher save(Dispatcher dispatcher);
+
+	public int delete(Long id);
+
+	public Dispatcher update(Long id, Dispatcher dispatcher);
+
+	public Dispatcher findByNom(String nom);
+
+	public Dispatcher findById(Long id);
+
+	public Dispatcher findByLogin(String login);
 
 }

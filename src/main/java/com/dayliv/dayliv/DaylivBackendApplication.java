e7cdd94e8b62d6f130a4b0d6b5405fd7105348c4
yassir78 <yassir.acaf@gmail.com>
2021-06-18 15:14:30 +0100

    yassir

--- src/main/java/com/dayliv/dayliv/DaylivBackendApplication.java
+++ src/main/java/com/dayliv/dayliv/DaylivBackendApplication.java
@@ -40,120 +40,99 @@ import com.dayliv.dayliv.model.SubCategory;
 import com.dayliv.dayliv.service.EmailService;
 
 @SpringBootApplication(scanBasePackages = "com.dayliv")
-public class DaylivBackendApplication implements CommandLineRunner  {
+public class DaylivBackendApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DaylivBackendApplication.class, args);
 	}
 
 	@Autowired
-    private EmailService emailService; 
-	
+	private EmailService emailService;
+
 	@Bean
 	public CommandLineRunner demo(ConsumerDao consumerDao, DispatcherDao dispatcherDao, LivreurDao livreurDao,
 			PartenaireDao partenaireDao, SuperAdminDao superAdminDao, ProductDao productDao,
 			IngredientDao ingredientDao, CommentaireDao commentaireDao, CommandeDao commandeDao,
 			CommandeItemDao commandeItemDao, CommandeStatusDao commandeStatusDao, PanierDao panierDao,
-			StoreDao storeDao, PanierItemDao panierItemDao, CategoryProductDao categoryProductDao,ProductImageDao productImageDao,
-			CategoryPartenaireDao categoryPartenaireDao, SubCategoryDao subCategoryDao) {
+			StoreDao storeDao, PanierItemDao panierItemDao, CategoryProductDao categoryProductDao,
+			ProductImageDao productImageDao, CategoryPartenaireDao categoryPartenaireDao,
+			SubCategoryDao subCategoryDao) {
 		return (args) -> {
-		/*	Commande commande1 = new Commande();
-			Commande commande2 = new Commande();
-			commande1.setReference("MT-2938");
-			commande1.setTotale_commande(4500F);
-			commande1.setDate_commande(new Date());
-			commande1.setLongitude(6.1431577F);
-			commande1.setLatitude(46.2043907F);
-			commande2.setLongitude(6.1431577F);
-			commande2.setLatitude(46.2043907F);
-			commande2.setReference("DF-9086");
-			commande2.setTotale_commande(9877F);
-			commande2.setDate_commande(new Date());
-			Stream.of(commande1, commande2).forEach(commande -> {
-				commandeDao.save(commande);
-			});*/
+			/*
+			 * Commande commande1 = new Commande(); Commande commande2 = new Commande();
+			 * commande1.setReference("MT-2938"); commande1.setTotale_commande(4500F);
+			 * commande1.setDate_commande(new Date()); commande1.setLongitude(6.1431577F);
+			 * commande1.setLatitude(46.2043907F); commande2.setLongitude(6.1431577F);
+			 * commande2.setLatitude(46.2043907F); commande2.setReference("DF-9086");
+			 * commande2.setTotale_commande(9877F); commande2.setDate_commande(new Date());
+			 * Stream.of(commande1, commande2).forEach(commande -> {
+			 * commandeDao.save(commande); });
+			 */
 			/* commande Item */
-			/*CommandeItem commandeItem1 = new CommandeItem();
-			CommandeItem commandeItem2 = new CommandeItem();
-			CommandeItem commandeItem3 = new CommandeItem();
-			commandeItem1.setQuantity(4);
-			commandeItem1.setTotal(4000F);
-			commandeItem1.setCommande(commande1);
-			commandeItem2.setQuantity(2);
-			commandeItem2.setTotal(35000F);
-			commandeItem2.setCommande(commande2);
-			commandeItem3.setQuantity(10);
-			commandeItem3.setTotal(7000F);
-			commandeItem3.setCommande(commande1);
-			Stream.of(commandeItem1, commandeItem2, commandeItem3).forEach(commandeItem -> {
-				commandeItemDao.save(commandeItem);
-			});
-			Stream.of(commandeItem1, commandeItem2).forEach(commandeItem -> {
-				commandeItemDao.save(commandeItem);
-			});*/
+			/*
+			 * CommandeItem commandeItem1 = new CommandeItem(); CommandeItem commandeItem2 =
+			 * new CommandeItem(); CommandeItem commandeItem3 = new CommandeItem();
+			 * commandeItem1.setQuantity(4); commandeItem1.setTotal(4000F);
+			 * commandeItem1.setCommande(commande1); commandeItem2.setQuantity(2);
+			 * commandeItem2.setTotal(35000F); commandeItem2.setCommande(commande2);
+			 * commandeItem3.setQuantity(10); commandeItem3.setTotal(7000F);
+			 * commandeItem3.setCommande(commande1); Stream.of(commandeItem1, commandeItem2,
+			 * commandeItem3).forEach(commandeItem -> { commandeItemDao.save(commandeItem);
+			 * }); Stream.of(commandeItem1, commandeItem2).forEach(commandeItem -> {
+			 * commandeItemDao.save(commandeItem); });
+			 */
 
 			/* commande status */
-			/*CommandeStatus commandeStatus1 = new CommandeStatus();
-			CommandeStatus commandeStatus2 = new CommandeStatus();
-			CommandeStatus commandeStatus3 = new CommandeStatus();
-			commandeStatus1.setEtat_commande("En cours");
-			commandeStatus2.setEtat_commande("Livrée");
-			commandeStatus3.setEtat_commande("En cours");
-			Stream.of(commandeStatus1, commandeStatus2, commandeStatus3).forEach(commandeStatus -> {
-				commandeStatusDao.save(commandeStatus);
-			});*/
-			/*commande1.setCommandeStatus(commandeStatus1);
-			commande2.setCommandeStatus(commandeStatus2);
-			Stream.of(commande1, commande2).forEach(commande -> {
-				commandeDao.save(commande);
-			});
+			/*
+			 * CommandeStatus commandeStatus1 = new CommandeStatus(); CommandeStatus
+			 * commandeStatus2 = new CommandeStatus(); CommandeStatus commandeStatus3 = new
+			 * CommandeStatus(); commandeStatus1.setEtat_commande("En cours");
+			 * commandeStatus2.setEtat_commande("Livrée");
+			 * commandeStatus3.setEtat_commande("En cours"); Stream.of(commandeStatus1,
+			 * commandeStatus2, commandeStatus3).forEach(commandeStatus -> {
+			 * commandeStatusDao.save(commandeStatus); });
+			 */
+			/*
+			 * commande1.setCommandeStatus(commandeStatus1);
+			 * commande2.setCommandeStatus(commandeStatus2); Stream.of(commande1,
+			 * commande2).forEach(commande -> { commandeDao.save(commande); });
+			 * 
+			 * Stream.of("XEHZJ8855", "KUISBJJS987", "OLKJHS96554").forEach(code -> { Store
+			 * store = new Store(); store.setCode(code); store.setPhone("06785521455");
+			 * store.setName("SHOP-" + code); store.setCodePostal("500300");
+			 * store.setInBusinessSince("2020"); store.setLat(5278.555);
+			 * store.setLg(56634.21); store.setEmail(code + "@gmail.com");
+			 * store.setAddress("MA" + code); store.setCurrency("CH");
+			 * store.setUseCache(true); store.setLogo(code + "logo.png");
+			 * 
+			 * storeDao.save(store);
+			 */
+			// categories
+			/*
+			 * Stream.of("pizza", "panini", "chawarma").forEach(libelle -> { CategoryProduct
+			 * categoryProduct = new CategoryProduct();
+			 * categoryProduct.setCategorie(libelle);
+			 * categoryProduct.setStoreCode("XEHZJ8855");
+			 * categoryProductDao.save(categoryProduct); Stream.of("s-pizza", "s-panini",
+			 * "s-chawarma").forEach(nom->{ SubCategory subCategory = new SubCategory();
+			 * subCategory.setSousCategorie(nom);
+			 * subCategory.setCategoryProduct(categoryProduct);
+			 * subCategoryDao.save(subCategory); }); }); });
+			 */
 
-			Stream.of("XEHZJ8855", "KUISBJJS987", "OLKJHS96554").forEach(code -> {
-				Store store = new Store();
-				store.setCode(code);
-				store.setPhone("06785521455");
-				store.setName("SHOP-" + code);
-				store.setCodePostal("500300");
-				store.setInBusinessSince("2020");
-				store.setLat(5278.555);
-				store.setLg(56634.21);
-				store.setEmail(code + "@gmail.com");
-				store.setAddress("MA" + code);
-				store.setCurrency("CH");
-				store.setUseCache(true);
-				store.setLogo(code + "logo.png");
-
-				storeDao.save(store);*/
-				// categories
-			/*	Stream.of("pizza", "panini", "chawarma").forEach(libelle -> {
-					CategoryProduct categoryProduct = new CategoryProduct();
-					categoryProduct.setCategorie(libelle);
-					categoryProduct.setStoreCode("XEHZJ8855");
-					categoryProductDao.save(categoryProduct);
-					Stream.of("s-pizza", "s-panini", "s-chawarma").forEach(nom->{
-						SubCategory subCategory = new SubCategory();
-						subCategory.setSousCategorie(nom);
-						subCategory.setCategoryProduct(categoryProduct);
-						subCategoryDao.save(subCategory);
-					});
-				});
-			});*/
-			
-			/*Stream.of("PC", "Imprimante", "Iphone", "Radio", "Clavier", "Clé USB", "Coffe", "Chaussures", "Table", "Chargeur", "Téléphone", "PC", "Imprimante", "Iphone", "Radio", "Clavier", "Clé USB", "Coffe", "Chaussures", "Table", "Chargeur", "Téléphone").forEach(libelle->{
-				Product product = new Product();
-				product.setLibelle(libelle);
-				product.setOrigine("Chine");
-				product.setStoreCode("XEHZJ8855");
-			    product.setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
-			    product.setPrice((float) 58.6);
-			    ProductImage pi = new ProductImage();
-			    productDao.save(product);
-			    pi.setPath("https://ma.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/39/358943/1.jpg?9484");
-			    pi.setProduct(product);
-			    productImageDao.save(pi);
-			});*/
-
-			
+			/*
+			 * Stream.of("PC", "Imprimante", "Iphone", "Radio", "Clavier", "Clé USB",
+			 * "Coffe", "Chaussures", "Table", "Chargeur", "Téléphone", "PC", "Imprimante",
+			 * "Iphone", "Radio", "Clavier", "Clé USB", "Coffe", "Chaussures", "Table",
+			 * "Chargeur", "Téléphone").forEach(libelle->{ Product product = new Product();
+			 * product.setLibelle(libelle); product.setOrigine("Chine");
+			 * product.setStoreCode("XEHZJ8855"); product.
+			 * setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+			 * ); product.setPrice((float) 58.6); ProductImage pi = new ProductImage();
+			 * productDao.save(product); pi.setPath(
+			 * "https://ma.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/39/358943/1.jpg?9484"
+			 * ); pi.setProduct(product); productImageDao.save(pi); });
+			 */
 
 		};
 
@@ -162,9 +141,9 @@ public class DaylivBackendApplication implements CommandLineRunner  {
 	@Override
 	public void run(String... args) throws Exception {
 
-          // emailService.sendMail("belkoweb9718@gmail.com", "Hi", "Ho ho ho");
-    	
-    	 //  emailService.sendPreConfiguredMail("Ho ho ho");
+		// emailService.sendMail("belkoweb9718@gmail.com", "Hi", "Ho ho ho");
+
+		// emailService.sendPreConfiguredMail("Ho ho ho");
 	}
 
 }

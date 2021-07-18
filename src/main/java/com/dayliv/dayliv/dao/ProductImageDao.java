26da13563af80dbc1e0b80390ad88fc49073691b
belkoweb <belkoweb9718@gmail.com>
2021-07-18 17:48:47 +0100

    done

--- src/main/java/com/dayliv/dayliv/dao/ProductImageDao.java
+++ src/main/java/com/dayliv/dayliv/dao/ProductImageDao.java
@@ -1,9 +1,10 @@
 package com.dayliv.dayliv.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.dayliv.dayliv.model.ProductImage;
-
+@Repository
 public interface ProductImageDao extends JpaRepository<ProductImage, Long> {
 
 }
